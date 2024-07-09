87cf87828b314879c3bd99c00613327661d93287
sS2S2bin <3dltnqls@naver.com>
2024-07-09 13:07:03 +0900

    초간단 서버, static 리소스 서버

--- ex02/app02.js
+++ ex02/app02.js
@@ -1,24 +1,22 @@
-const { fstat, readFile } = require('fs');
 var http = require('http');
 var fs = require('fs');
 
 var port = 9090;
-var server = http.createServer(function (req, res) {
+var server = http.createServer(function(req, res) {
     console.log(req.url);
-    if(req.url === '/'){
+
+    if(req.url === '/') {
         req.url = '/index.html';
     }
-    fs.readFile(__dirname + '/public'+req.url,function(error, data) {
-        res.writeHead(200,{ // 200 응답, header 내용
+
+    fs.readFile(__dirname + '/public' + req.url, function(error, data) {
+        res.writeHead(200, {
             'Content-Type': 'text/html'
         });
-        res.end('<h1>Hell WorlD</h1>'); //body 내용        
+        res.end(data);    
     });
-
-    
 });
 
-// 서버 실행
-server.listen(port,function () {
-    console.log('server: starts.... ['+port+']');
+server.listen(port, function(){
+    console.log('server: starts....[' + port + ']');
 });
--- ex02/readme.txt
+++ ex02/readme.txt
@@ -1,3 +1,3 @@
-node server programing examples
-1) app01.js
-2) app02.js
+node server programming examples
+1) app01.js: 초간단 서버
+2) app02.js: static 리소스 서버
